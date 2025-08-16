704339cbf7777076eaf60efdb2292e1248bd78d8
RobinsAviary <robinsaviary@gmail.com>
2025-08-16 10:01:45 -0400

    init

---
+++ Opcodes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shitty502
+{
+    internal static class Opcodes
+    {
+        public static KeyValuePair<string,string>[] opcodes = {
+            new("SetA", "LDA"),
+            new("SetX", "LDX"),
+            new("SetY", "LDY"),
+            new("StoreA", "STA"),
+            new("StoreX", "STX"),
+            new("StoreY", "STY"),
+            new("TransferAX", "TAX"),
+            new("TransferAY", "TAY"),
+            new("TransferSX", "TSX"),
+            new("TransferXA", "TXA"),
+            new("TransferXS", "TXS"),
+            new("TransferYA", "TYA"),
+            new("Dec", "DEC"),
+            new("Inc", "INC"),
+            new("IncX", "INX"),
+            new("IncY", "INY"),
+            new("DecX", "DEX"),
+            new("DecY", "DEY"),
+            new("Add", "ADC"),
+            new("Sub", "SBC"),
+            new("And", "AND"),
+            new("Eor", "EOR"),
+            new("Or", "ORA"),
+            new("ShiftLeft", "ASL"),
+            new("ShiftRight", "LSR"),
+            new("RotateLeft", "ROL"),
+            new("RotateRight", "ROR"),
+            new("ClearCarry", "CLC"),
+            new("DisableDecimal", "CLD"),
+            new("EnableInterrupts", "CLI"),
+            new("ClearOverflow", "CLV"),
+            new("SetCarry", "SEC"),
+            new("EnableDecimal", "SED"),
+            new("DisableInterrupts", "SEI"),
+            new("CompareA", "CMP"),
+            new("CompareX", "CPX"),
+            new("CompareY", "CPY"),
+            new("BranchNotCarry", "BCC"),
+            new("BranchCarry", "BCS"),
+            new("BranchEqual", "BEQ"),
+            new("BranchNotEqual", "BNE"),
+            new("BranchPositive", "BPL"),
+            new("BranchNegative", "BMI"),
+            new("BranchOverflow", "BVS"),
+            new("BranchNotOverflow", "BVC"),
+            new("Jump", "JMP"),
+            new("JumpSub", "JSR"),
+            new("ReturnSub", "RTS"),
+            new("Break", "BRK"),
+            new("ReturnInterrupt", "RTI"),
+            new("Bittest", "BIT"),
+            new("Nope", "NOP"),
+            new("PushA", "PHA"),
+            new("PushPPR", "PHP"),
+            new("PullA", "PLA"),
+            new("PullPPR", "PLP"),
+        };
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Shitty502;
+
+string name = "Shitty502";
+string errorSuffix = "Error: ";
+string crossparserError = name + " Cross-Parser " + errorSuffix;
+string assemblySourceExt = ".s";
+string outputFile = "output" + assemblySourceExt;
+
+if (args.Length > 0)
+{
+    string filename = args.First();
+
+    if (args.Length > 1) {
+        outputFile = args[1];
+        if (!outputFile.EndsWith(assemblySourceExt))
+        {
+            outputFile += assemblySourceExt;
+        }
+    }
+
+    if (File.Exists(filename))
+    {
+        if (filename.EndsWith(".s"))
+        {
+            string output = "";
+
+            var lines = File.ReadLines(filename);
+            foreach (string line in lines)
+            {
+                foreach (var pair in Opcodes.opcodes)
+                {
+                    if (line.StartsWith(pair.Key))
+                    {
+                        output += line.Replace(pair.Key, pair.Value) + "\n";
+
+                        break;
+                    }
+                }
+            }
+
+            output = output.TrimEnd('\n');
+
+            File.WriteAllText(outputFile, output);
+        }
+        else
+        {
+            Console.WriteLine(crossparserError + $"File specified is not an assembly source file ({assemblySourceExt}).");
+        }
+    } else
+    {
+        Console.WriteLine(crossparserError + "File does not exist.");
+    }
+} else
+{
+    Console.WriteLine(crossparserError + "No argument/file provided.");
+}
